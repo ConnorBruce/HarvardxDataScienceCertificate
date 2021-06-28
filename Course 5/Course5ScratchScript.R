dd46ac734fe43c5e0a6e5b4f5a53e8b9171d31d0
ConnorBruce <iconnorbruce@gmail.com>
2021-06-28 12:56:14 -0400

    Section 4 stuff

--- Course 5/Course5ScratchScript.R
+++ Course 5/Course5ScratchScript.R
@@ -9,3 +9,18 @@ library(dplyr)
 library(RColorBrewer)
 library(gtools)
 
+getwd()
+url = "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dest_file = "Course 5/murders/data/murders.csv"
+download.file(url,destfile=dest_file)
+
+murders = read_csv("Course 5/murders/data/murders.csv")
+murders = murders %>% mutate(region = factor(region), rate = total/population *10^5)
+save(murders, file = "Course 5/murders/rda/murders.rda")
+
+murders %>% mutate(abb = reorder(abb, rate)) %>%
+  ggplot(aes(abb,rate)) +
+  geom_bar(width = .5, stat = "identity", color = "black") +
+  coord_flip()
+
+ggsave("Course 5/murders/figs/barplot.png")
